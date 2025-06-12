ac53120dc2e6f6c80e1caf0a58665b4a79156cfb
Ilia <Rubiliam2003@gmail.com>
2025-06-12 16:25:00 -0700

    Add subsections to resume page

--- src/web/wsgi.py
+++ src/web/wsgi.py
@@ -17,4 +17,4 @@ app = create_app(Config)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=WEB_CONTAINER_PORT)
+    app.run(host="0.0.0.0", port=WEB_CONTAINER_PORT, debug=True, use_reloader=True)
